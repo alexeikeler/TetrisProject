95c9ab5a764a3a815b3e27acf9e2856451ea2643
alex <keleralexei@gmail.com>
2024-07-09 19:40:16 +0200

    Return from failed collision change

--- MockTerminalManager.cpp
+++ MockTerminalManager.cpp
@@ -1,27 +1,4 @@
 // Copyright: 2024 by Ioan Oleksii Kelier keleralexei@gmail.com
 // Code snippets from the lectures where used
 
-#include "./MockTerminalManager.h"
-#include "./Point.h"
-
-
-MockTerminalManager::MockTerminalManager(int numRows, int numCols)
-{
-    numRows_ = numRows;
-    numCols_ = numCols;
-}
-
-void MockTerminalManager::drawPixel(int row, int col, int color)
-{
-    drawnPixels_[Point{row, col}] = color;
-}
-
-void MockTerminalManager::refresh()
-{
-    return;
-}
-
-bool MockTerminalManager::isPixelDrawn(int row, int col) const
-{
-    return drawnPixels_.count(Point{row, col});
-}
+// Currently empty, but will be added later
